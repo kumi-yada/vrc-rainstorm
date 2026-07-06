9cc8e4dc79f501822eecec9afae0ca008c38d76a
myin <minyin142p@gmail.com>
2026-07-06 10:48:36 +0200

    init night + reduce light amount

--- Room/NightToggle.cs
+++ Room/NightToggle.cs
@@ -10,15 +10,20 @@ public class NightToggle : UdonSharpBehaviour
 
     void Start()
     {
-        
+        SetNight(true);
     }
 
     public void ToggleNight()
     {
         Debug.Log("Toggling night mode");
         bool isOff = RenderSettings.ambientIntensity < 0.1f;
-        RenderSettings.ambientIntensity = isOff ? 0.3f : 0f;
-        RenderSettings.skybox.SetFloat("_Exposure", isOff ? 0.3f : 0f);
-        lightObject.SetActive(isOff);
+        SetNight(!isOff);
+    }
+
+    private void SetNight(bool isNight)
+    {
+        RenderSettings.ambientIntensity = isNight ? 0f : 0.3f;
+        RenderSettings.skybox.SetFloat("_Exposure", isNight ? 0f : 0.3f);
+        lightObject.SetActive(!isNight);
     }
 }
